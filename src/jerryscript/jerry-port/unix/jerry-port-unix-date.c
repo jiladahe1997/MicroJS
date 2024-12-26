694bb979b4872e5b55a9455cadf656530c062fb1
jiladahe1997 <jiladahe1997@gmail.com>
2024-12-26 17:08:59 +0800

    add: add jerry-script/jerry-port into compile

--- src/jerryscript/jerry-port/unix/jerry-port-unix-date.c
+++ src/jerryscript/jerry-port/unix/jerry-port-unix-date.c
@@ -15,7 +15,6 @@
 
 #include "jerryscript-port.h"
 
-#if defined(__unix__) || defined(__APPLE__)
 
 #include <sys/time.h>
 #include <time.h>
@@ -23,37 +22,39 @@
 int32_t
 jerry_port_local_tza (double unix_ms)
 {
-  time_t time = (time_t) unix_ms / 1000;
+//   time_t time = (time_t) unix_ms / 1000;
 
-#if defined(HAVE_TM_GMTOFF)
-  struct tm tm;
-  localtime_r (&time, &tm);
+// #if defined(HAVE_TM_GMTOFF)
+//   struct tm tm;
+//   localtime_r (&time, &tm);
 
-  return ((int32_t) tm.tm_gmtoff) * 1000;
-#else /* !defined(HAVE_TM_GMTOFF) */
-  struct tm gmt_tm;
-  struct tm local_tm;
+//   return ((int32_t) tm.tm_gmtoff) * 1000;
+// #else /* !defined(HAVE_TM_GMTOFF) */
+//   struct tm gmt_tm;
+//   struct tm local_tm;
 
-  gmtime_r (&time, &gmt_tm);
-  localtime_r (&time, &local_tm);
+//   gmtime_r (&time, &gmt_tm);
+//   localtime_r (&time, &local_tm);
 
-  time_t gmt = mktime (&gmt_tm);
+//   time_t gmt = mktime (&gmt_tm);
 
-  /* mktime removes the daylight saving time from the result time value, however we want to keep it */
-  local_tm.tm_isdst = 0;
-  time_t local = mktime (&local_tm);
+//   /* mktime removes the daylight saving time from the result time value, however we want to keep it */
+//   local_tm.tm_isdst = 0;
+//   time_t local = mktime (&local_tm);
 
-  return (int32_t) difftime (local, gmt) * 1000;
-#endif /* HAVE_TM_GMTOFF */
+//   return (int32_t) difftime (local, gmt) * 1000;
+// #endif /* HAVE_TM_GMTOFF */
+    (void)unix_ms;
+    return 0;
 } /* jerry_port_local_tza */
 
 double
 jerry_port_current_time (void)
 {
-  struct timeval tv;
-  gettimeofday (&tv, NULL);
+//   struct timeval tv;
+//   gettimeofday (&tv, NULL);
 
-  return ((double) tv.tv_sec) * 1000.0 + ((double) tv.tv_usec) / 1000.0;
+//   return ((double) tv.tv_sec) * 1000.0 + ((double) tv.tv_usec) / 1000.0;
+    return 0;
 } /* jerry_port_current_time */
 
-#endif /* defined(__unix__) || defined(__APPLE__) */
